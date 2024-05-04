26305ae95e974ec81eafd8e0d79bf2da109a4815
OTeeEnabor <tobienabor@gmail.com>
2024-05-04 11:50:31 +0200

    scraping product data from scraped urls is working.

--- main.py
+++ main.py
@@ -1,17 +1,45 @@
+import itertools
 import os
 import pprint
 from datetime import date
+import concurrent.futures
+import time
 
+import numpy as np
+import pandas as pd
 from web_scrape import helpers, selenium_scrape, store_scrape
+from web_scrape.selenium_scrape import get_product_data
 
+start = time.perf_counter()
 
-store_scraper = store_scrape.StoreScraper()
+# # initialise store scraper
+store_scraper = store_scrape.StoreScraper(date_stamp="2024426")
+print(store_scraper.date_stamp)
+# get the url csvs
+product_url_dict = store_scraper.get_product_csv_dict(
+    scrape_date="2024426"
+)  # get_product_urls()
 
-# # get current date
-# store_scraper.current_date
-# # get store information dictionary
-# print(store_scraper.store_information_dict)
-# # print the driver object
-# print(store_scraper.driver)
+for store, csv_list in product_url_dict.items():
+    if __name__ == "__main__":
+        with concurrent.futures.ProcessPoolExecutor() as executor:
+            results = executor.map(get_product_data, csv_list)
+            output_list = []
+            for result in results:
+                list_result = list(result)
+                print(list_result)
+                output_list.append(list_result)
+        results = list(np.concatenate(output_list).flat)
+        # create output csv
+        product_data_df = pd.DataFrame(results)
+        print(product_data_df.head())
+        # # save to csv
+        product_data_df.to_csv(
+            store_scraper.storage_path
+            + f"{store}\\{store_scraper.date_stamp}\\{store}_product_data\\products_data_{store_scraper.date_stamp}.csv",
+            index=False,
+        )
 
-print(store_scraper.get_product_csv_list(date="2024426"))  # get_product_urls()
+finish = time.perf_counter()
+
+print(f"finished in {round(finish-start,2)} seconds (s)")
